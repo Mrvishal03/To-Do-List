d86d5bdfbe6301ee900c8206cce6708a9ec421a7
root <root@DESKTOP-N6IQJA4>
2023-11-17 15:11:38 +0530

    To DO List

--- src/to_do_list_frontend/src/index.js
+++ src/to_do_list_frontend/src/index.js
@@ -1,19 +1,62 @@
-import { to_do_list_backend } from "../../declarations/to_do_list_backend";
+ import { to_do_list_backend } from "../../declarations/to_do_list_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+const textarea = document.querySelector(".textarea");
+const appointForm = document.querySelector(".inputcol");
+
+appointForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
+  const obj = {
+    textarea: textarea.value,
+  };
 
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await to_do_list_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+  try {
+    const getId = await to_do_list_backend.addTodo(obj.textarea);
+    showUserOnScreen({ id: getId, textarea: obj.textarea });
+    textarea.value = ""; 
+  } catch (error) {
+    console.error("Error adding todo:", error);
+  }
 });
+
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const getAll = await to_do_list_backend.getAllTodo();
+    getAll.forEach((todo) => showUserOnScreen(todo));
+  } catch (error) {
+    console.error("Error fetching todo list:", error);
+  }
+});
+
+function showUserOnScreen(user) {
+  const parentNode = document.getElementById("todolist");
+
+  const newListItem = document.createElement("li");
+  newListItem.className = "item";
+  newListItem.id = Number(user.id);
+  newListItem.textContent = user.textarea;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "trash-button";
+  deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  deleteButton.onclick = () => deleteTodo(user.id);
+
+  newListItem.appendChild(deleteButton);
+  parentNode.appendChild(newListItem);
+}
+
+async function deleteTodo(userId) {
+  try {
+    console.log("userId in del =>", userId);
+    await to_do_list_backend.deleteTodo(userId);
+    removeUserFromScreen(userId);
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+  }
+}
+
+function removeUserFromScreen(userId) {
+  const childNodeToBeDeleted = document.getElementById(userId);
+  if (childNodeToBeDeleted) {
+    childNodeToBeDeleted.parentNode.removeChild(childNodeToBeDeleted);
+  }
+}
